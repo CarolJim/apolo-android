9fe6d239d976a72cdd1f4d338c89fd5f1ccd585d
Roberto Vargas <rvargas@pagatodo.com>
2017-07-27 09:50:09 -0500

    onSaveInstanceState and onRestoreInstanceState for data persistent (celular, telefono, tarjeta, ine frente, ine vuelta). HashMap for solicitud.

---
+++ app/src/main/java/com/pagatodo/apolo/data/local/SharedPreference.java
@@ -0,0 +1,8 @@
+package com.pagatodo.apolo.data.local;
+
+/**
+ * Created by rvargas on 26/07/2017.
+ */
+
+public class SharedPreference {
+}
---
+++ app/src/main/java/com/pagatodo/apolo/data/pojo/Afiliado.java
@@ -0,0 +1,8 @@
+package com.pagatodo.apolo.data.pojo;
+
+/**
+ * Created by rvargas on 26/07/2017.
+ */
+
+public class Afiliado {
+}
